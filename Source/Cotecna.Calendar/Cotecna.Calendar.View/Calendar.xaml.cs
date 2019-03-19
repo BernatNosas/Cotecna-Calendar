0c25765e8fd78623795ce5d933b58439457cf84c
Bernat Nosas <45341579+BNosas@users.noreply.github.com>
2019-03-20 00:52:42 +0100

    Add files via upload

---
+++ Source/Cotecna.Calendar/Cotecna.Calendar.View/Calendar.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Cotecna.Calendar.View.ViewModel;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Cotecna.Calendar.View
+{
+    /// <summary>
+    /// Interaction logic for Calendar.xaml
+    /// </summary>
+    public partial class Calendar : Window
+    {
+
+        #region constructors
+
+        public Calendar()
+        {
+            this.DataContext = new CalendarVM();
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region methods
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            CalendarVM c = (CalendarVM)this.DataContext;
+            ICommand com = c.CalendarCommand;
+            com.Execute(this.gCalendar);
+        }
+
+        #endregion
+
+    }
+}
