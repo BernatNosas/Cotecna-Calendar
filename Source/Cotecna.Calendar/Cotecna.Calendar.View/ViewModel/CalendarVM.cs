8e17dc36de682243bbd68c4222318d36098fa604
Bernat Nosas <45341579+BNosas@users.noreply.github.com>
2019-03-20 00:57:55 +0100

    Add files via upload

---
+++ Source/Cotecna.Calendar/Cotecna.Calendar.View/ViewModel/CalendarVM.cs
@@ -0,0 +1,83 @@
+﻿using Cotecna.Calendar.Model.Enums;
+using Cotecna.Calendar.View.Commands;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace Cotecna.Calendar.View.ViewModel
+{
+    public class CalendarVM
+    {
+
+        #region constants
+
+        private const int INITIAL_YEAR = 1970;
+
+        #endregion
+
+        #region fields
+
+        private eMonths month;
+        private int year;
+        private ObservableCollection<int> avalibleYears;
+        private GetCalendarCommand calendarCommand;
+
+        #endregion
+
+        #region constructors
+
+        public CalendarVM()
+        {
+            this.Month = (eMonths)DateTime.Now.Month;      
+            this.SetAvalibleYears();
+            this.year = DateTime.Now.Year;
+         
+        }
+
+        #endregion
+
+        #region properties
+        public IEnumerable<eMonths> Months => Enum.GetValues(typeof(eMonths)).Cast<eMonths>();
+
+        public ObservableCollection<int> Years => this.avalibleYears;
+
+        public eMonths Month {
+            get { return this.month; }
+            set { this.month = value; }
+        }
+
+        public int Year
+        {
+            get { return this.year; }
+            set { this.year = value; }
+        }
+
+        public GetCalendarCommand CalendarCommand
+        {
+            get
+            {
+                if(this.calendarCommand == null)
+                {
+                    this.calendarCommand = new GetCalendarCommand(this);
+                }
+                return this.calendarCommand;
+            }
+        }
+
+        #endregion
+
+        #region methods
+
+        private void SetAvalibleYears()
+        {
+            avalibleYears = new ObservableCollection<int>();
+            for (int i = INITIAL_YEAR; i <= DateTime.Now.Year; i++)
+            {
+                avalibleYears.Add(i);
+            }
+        }
+
+        #endregion
+    }
+}
