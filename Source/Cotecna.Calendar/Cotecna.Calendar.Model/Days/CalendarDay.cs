042cca58304a8c796b8847b724821b32e537eaf5
Bernat Nosas <45341579+BNosas@users.noreply.github.com>
2019-03-20 07:15:09 +0100

    Add files via upload

---
+++ Source/Cotecna.Calendar/Cotecna.Calendar.Model/Days/CalendarDay.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Globalization;
+
+namespace Cotecna.Calendar.Model.Days
+{
+    public class CalendarDay
+    {
+
+        #region fields
+
+        private DateTime currentDate;
+        private int weekNumber;
+        private string forecast;
+
+        #endregion
+
+        #region constructors
+
+        public CalendarDay(int d,int m, int y)
+        {
+            this.currentDate = new DateTime(y, m, d);
+            this.forecast = string.Empty;
+            this.GetWeekNumber();
+        }
+
+        #endregion
+
+        #region properties
+
+        public int DayNumber
+        {
+            get { return this.currentDate.Day; }
+        }
+
+        public DayOfWeek CurrentDay
+        {
+            get { return this.currentDate.DayOfWeek; }
+        }
+
+        public int WeekNumber
+        {
+            get { return this.weekNumber; }
+        }
+
+        public bool IsTodaySelected
+        {
+            get { return this.currentDate.Date.Equals(DateTime.Now.Date); }
+        }
+
+        public string Forecast
+        {
+            get { return this.forecast; }
+            set { this.forecast = value; }
+        }
+
+        #endregion
+
+        #region methods
+
+        private void GetWeekNumber()
+        {
+            CultureInfo cul = CultureInfo.CurrentCulture;
+            this.weekNumber = cul.Calendar.GetWeekOfYear(this.currentDate,
+                 CalendarWeekRule.FirstDay,
+                 DayOfWeek.Monday);
+        }
+
+        #endregion
+
+    }
+}
---
+++ Source/Cotecna.Calendar/Cotecna.Calendar.Model/Days/CalendarMonth.cs
@@ -0,0 +1,74 @@
+﻿using Cotecna.Calendar.Model.Forecast;
+using System;
+using System.Collections.Generic;
+using System.Json;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cotecna.Calendar.Model.Days
+{
+    public class CalendarMonth
+    {
+
+        #region constructors
+
+        private CalendarMonth() { }
+
+        #endregion
+
+        #region methods
+
+        public static async Task<List<CalendarDay>> GetCalendarMonth (int year, int month)
+        {
+            int d = DateTime.DaysInMonth(year, month);
+            List<CalendarDay> days = new List<CalendarDay>();
+            for (int i = 1; i <= d; i++)
+            {
+                days.Add(new CalendarDay(i, month, year));
+            }
+            if (DateTime.Now.Month == month && DateTime.Now.Year == year)
+            {
+                await CalendarMonth.SetWeatherForecast(days);
+            }
+            return days;
+        }
+
+        private static async Task SetWeatherForecast(List<CalendarDay> days)
+        {
+            IEnumerable<CalendarDay> calen = days.Where(d => 
+                                                        d.DayNumber > DateTime.Now.Day &&
+                                                        d.DayNumber < DateTime.Now.AddDays(6).Day).ToList();
+            JsonValue result = await WeatherForecast.GetWeatherForecast();
+            if (result != null)
+            {
+                CalendarMonth.SetWeatherForecastPerDay(days, (JsonArray)result["list"]);
+            }
+            else
+            {
+             foreach(CalendarDay c in calen)
+                {
+                    c.Forecast = "Unavalible";
+                }
+            }
+
+        }
+
+        private static void SetWeatherForecastPerDay(List<CalendarDay> days, JsonArray result)
+        {
+            var filteredResults = from j in result
+                          group j by DateTime.Parse(j["dt_txt"]).Day into g
+                          select new { ForecastDay = g.Key, Forecasts = g.ToList() };
+
+            foreach(var ds in filteredResults)
+            {
+                CalendarDay sc = days.Where(d => d.DayNumber == ds.ForecastDay).FirstOrDefault();
+                var temp = (int)(ds.Forecasts[0]["main"]["temp"] - 273.15);
+                sc.Forecast = ds.Forecasts[0]["weather"][0]["main"] + "(" + temp.ToString() + "ºC)";
+            }
+        }
+
+        #endregion
+
+    }
+}
